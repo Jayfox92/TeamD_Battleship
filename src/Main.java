42fc6561ca63195103fd78d534f618931a5b3680
Safa97G <safa.akan.JAVA23@edu.edugrade.se>
2023-10-27 13:15:36 +0200

    ändrat på main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import java.sql.SQLOutput;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("hej på er");
+        String x = "Jag är hungring vad finns det att äta ?";
+        String y = "Korv med mos";
 
-        }
+
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }

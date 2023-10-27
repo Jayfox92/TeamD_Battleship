2c432cc3d09d757f1580d6aea1c3aa64bcede8f4
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-10-27 13:29:48 +0200

    ny master med 2d array

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,15 @@ public class Main {
                 {'@','@','@','@','@','@','@','@','@','@'},
         };
 
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+            System.out.println("test");
+            System.out.println("test2");
+            System.out.println("test3");
+        }
+
     }
 }

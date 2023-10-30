ff809aa0270e79a872a5f1211b2a0a3046f011ce
Petter Lindh <petter.s.lindh@gmail.com>
2023-10-30 21:40:49 +0100

    test4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,7 +24,10 @@ public class Main {
             System.out.println("test");
             System.out.println("test2");
             System.out.println("test3");
+            System.out.println("test"+(i-1));
         }
+
+
 
     }
 }
